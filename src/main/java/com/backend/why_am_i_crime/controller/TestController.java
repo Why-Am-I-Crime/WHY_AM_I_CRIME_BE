1ee7e8873711ee13d62e01a2699d8bc4dafa926d
mijeong <victon777@naver.com>
2024-07-16 18:22:28 +0900

    Init: 개발 환경 세팅

--- src/main/java/com/backend/why_am_i_crime/controller/TestController.java
+++ src/main/java/com/backend/why_am_i_crime/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/")
 public class TestController {
+
     @GetMapping()
     public String hello(){
         return "Hello World";
