d5e2ac5e2ca050823f86911e31ff5e5d28946be7
mmije0ng <victon777@naver.com>
2024-07-21 17:12:25 +0900

    Feat: Custom Exception 처리

--- src/main/java/com/backend/why_am_i_crime/exception/NoSuchDataException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.why_am_i_crime.exception;
-
-public class NoSuchDataException extends RuntimeException{
-    public NoSuchDataException(String message) {
-        super(message);
-    }
-}
